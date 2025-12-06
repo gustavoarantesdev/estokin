5dc20e8cfce3f9bf57851a7ad133e344e657ff89
Gustavo Arantes <gustavoarantesdev@gmail.com>
2025-12-05 21:15:33 -0300

    feat: adiciona rota de logout

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,18 @@
 <?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return redirect()->to('/login');
+});
+
+// Rotas para usuários não autenticado.
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/login', [AuthController::class, 'create'])->name('login');
+    Route::post('/login', [AuthController::class, 'store']);
+});
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/logout', [AuthController::class, 'destroy'])->name('logout');
+});
