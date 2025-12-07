bc5cf2239ee667930af0dd4485d31f7b24013c72
Gustavo Arantes <gustavoarantesdev@gmail.com>
2025-12-07 06:36:12 -0300

    feat: adiciona rotas para produto

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,14 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->to('/login');
 });
 
-// Rotas para usuários não autenticado.
+// // Rotas para usuários não autenticado.
 Route::group(['middleware' => 'guest'], function () {
     Route::get('/login', [AuthController::class, 'create'])->name('login');
     Route::post('/login', [AuthController::class, 'store']);
@@ -18,4 +19,6 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/logout', [AuthController::class, 'destroy'])->name('logout');
 
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+
+    Route::resource('produto', ProductController::class);
 });
