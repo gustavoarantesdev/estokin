96bfb6b21f3e14f6de48b82a6cf7768c31bb15bd
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-06-15 14:59:33 -0300

    feat: adiciona componentes no layout master

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "@/app/ui/globals.css";
 import { inter } from "@/app/ui/fonts";
+import { EstokinLogo } from "@/app/ui/estokin-logo";
+import { NavigationBar } from "@/app/ui/layout/navigation-bar";
 
 export const metadata: Metadata = {
   title: "Estokin",
@@ -14,7 +16,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR" className={`${inter.className} h-full antialiased`}>
-      <body className="">{children}</body>
+      <body className="flex justify-center items-center p-4 h-screen">
+        <main className="bg-slate-50 shadow-lg p-4 w-full sm:max-w-lg h-screen">
+          {/* Header */}
+          <div className="relative flex flex-col bg-white shadow-md border border-gray-300 rounded-xl h-full">
+            <div className="shrink-0">
+              <EstokinLogo />
+              <hr className="text-slate-300"></hr>
+            </div>
+
+            {/* Content */}
+            <div className="flex-1 p-4 pb-28 overflow-auto scroll-smooth">
+              {children}
+            </div>
+
+            {/* Navigation bar */}
+            <NavigationBar />
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
