fc1b8e1afbc6e16bcd4197e3bbb1a4f9c92b57d5
Gustavo Arantes <gustavoarantesdev@gmail.com>
2026-06-15 23:45:02 -0300

    feat: adiciona dropdown

--- src/app/ui/layout/navigation-bar.tsx
+++ src/app/ui/layout/navigation-bar.tsx
@@ -4,12 +4,14 @@ import clsx from "clsx";
 import { Home, FileBox, Plus, PackageSearch, Menu } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 export function NavigationBar() {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="absolute bottom-0 z-50 w-full rounded-br-xl rounded-bl-xl border-t border-slate-300 bg-white">
-      <ul className="flex h-20 items-center justify-between mx-4 space-x-3 md:space-x-4 lg:space-x-6">
+      <ul className="mx-4 flex h-20 items-center justify-between space-x-3 md:space-x-4 lg:space-x-6">
         <li>
           <Link
             href={"/dashboard"}
@@ -34,12 +36,44 @@ export function NavigationBar() {
         </li>
 
         <li className="relative flex items-center justify-center">
-          <Link
-            href={"/menu"}
-            className="my-transition cursor-pointer rounded-full bg-purple-100 p-4 text-purple-500 hover:bg-purple-400 hover:text-white active:bg-purple-500"
+          <button
+            onClick={() => setIsOpen((prev) => !prev)}
+            className="my-transition cursor-pointer touch-manipulation rounded-full bg-purple-100 p-4 text-purple-500 select-none hover:bg-purple-400 hover:text-white active:bg-purple-500"
           >
-            <Plus />
-          </Link>
+            <Plus className="pointer-events-none" />
+          </button>
+
+          {isOpen && (
+            <div className="absolute bottom-full left-1/2 z-50 mb-5 w-64 -translate-x-1/2 rounded-xl border border-slate-300 bg-slate-50 p-4">
+              <ul>
+                <li className="space-y-3">
+                  <p className="text-sm font-medium text-slate-600">Produto</p>
+                  <Link
+                    href={"/products/create"}
+                    className="my-transition block w-full cursor-pointer rounded-xl border border-slate-300 bg-slate-100 p-2 text-center text-slate-600 hover:border-purple-300 hover:bg-purple-100 hover:text-purple-600 active:bg-purple-400 active:text-white"
+                  >
+                    Cadastrar
+                  </Link>
+                </li>
+                <hr className="my-3 text-slate-300" />
+                <li className="space-y-3">
+                  <p className="text-sm font-medium text-slate-600">Estoque</p>
+                  <Link
+                    href={"/stocks/insert"}
+                    className="my-transition block w-full cursor-pointer rounded-xl border border-green-300 bg-green-100 p-2 text-center text-green-600 hover:border-green-300 hover:bg-green-200 hover:text-green-600 active:bg-green-400 active:text-white"
+                  >
+                    Entrada
+                  </Link>
+                  <Link
+                    href={"/stocks/output"}
+                    className="my-transition block w-full cursor-pointer rounded-xl border border-red-300 bg-red-100 p-2 text-center text-red-600 hover:border-red-300 hover:bg-red-200 hover:text-red-600 active:bg-red-400 active:text-white"
+                  >
+                    Saída
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
         </li>
 
         <li>
